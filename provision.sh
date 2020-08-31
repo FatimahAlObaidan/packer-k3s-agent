8c65e107beabe81ec39dbf6a4219c8ed85e080d1
Ubuntu <ubuntu@ip-10-0-1-146.me-south-1.compute.internal>
2020-08-31 21:37:26 +0000

    staying up

--- provision.sh
+++ provision.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update -y && sudo apt install -y curl vim jq git make docker.io
 sudo usermod -aG docker ubuntu
-TOKEN='omega'
+TOKEN=123
 export K3S_HOST=controlplane.omega.dobc
 export K3S_TOKEN=$TOKEN
 export K3S_URL=https://$K3S_HOST:6443
